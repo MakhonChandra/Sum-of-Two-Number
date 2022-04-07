8c2c2bf6dc79220e901733b04a23ad7e605df327
Makhon Chandra Barman <103095870+MakhonChandra@users.noreply.github.com>
2022-04-07 11:40:00 -0700

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  7 11:33:20 2022
+
+@author: Makhon Chandra
+"""
+
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Secound Number:"))
+result = num1 + num2
+print(num1,"+",num2,"=",result)
